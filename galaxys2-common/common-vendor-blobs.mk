6ec3e2289ca0d61017fa8429bc032855642d1825
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-11-21 22:56:37 +0100

    galaxys2: use correct audio configs

--- galaxys2-common/common-vendor-blobs.mk
+++ galaxys2-common/common-vendor-blobs.mk
@@ -62,16 +62,39 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/asound.conf:system/etc/asound.conf \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/LVVEFS_Rx_Configuration.txt:system/etc/audio/LVVEFS_Rx_Configuration.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/LVVEFS_Tx_Configuration.txt:system/etc/audio/LVVEFS_Tx_Configuration.txt \
-    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Rx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Rx_ControlParams_WIRED_HEADSET_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_WIRED_HEADSET_WIDEBAND.txt \
-    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Tx_ControlParams_EARPIECE_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_EARPIECE_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt \
     vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/FMRadioEar.ini:system/etc/audio/codec/FMRadioEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/FMRadioSpk.ini:system/etc/audio/codec/FMRadioSpk.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/MusicEar.ini:system/etc/audio/codec/MusicEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/MusicSpk.ini:system/etc/audio/codec/MusicSpk.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/RecHeadSetMic.ini:system/etc/audio/codec/RecHeadSetMic.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/RecMainMic.ini:system/etc/audio/codec/RecMainMic.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/RecSubMic.ini:system/etc/audio/codec/RecSubMic.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/RingtoneEar.ini:system/etc/audio/codec/RingtoneEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/RingtoneSpk.ini:system/etc/audio/codec/RingtoneSpk.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceCall3pEar.ini:system/etc/audio/codec/VoiceCall3pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceCall4pEar.ini:system/etc/audio/codec/VoiceCall4pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceCallBT.ini:system/etc/audio/codec/VoiceCallBT.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceCallRcv.ini:system/etc/audio/codec/VoiceCallRcv.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceCallSpk.ini:system/etc/audio/codec/VoiceCallSpk.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceRecHeadSetMic.ini:system/etc/audio/codec/VoiceRecHeadSetMic.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoiceRecMainMic.ini:system/etc/audio/codec/VoiceRecMainMic.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoipCall3pEar.ini:system/etc/audio/codec/VoipCall3pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoipCall4pEar.ini:system/etc/audio/codec/VoipCall4pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoipCallRcv.ini:system/etc/audio/codec/VoipCallRcv.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VoipCallSpk.ini:system/etc/audio/codec/VoipCallSpk.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VtCall3pEar.ini:system/etc/audio/codec/VtCall3pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VtCall4pEar.ini:system/etc/audio/codec/VtCall4pEar.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VtCallBT.ini:system/etc/audio/codec/VtCallBT.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VtCallRcv.ini:system/etc/audio/codec/VtCallRcv.ini \
+    vendor/samsung/galaxys2-common/proprietary/audio/etc/audio/codec/VtCallSpk.ini:system/etc/audio/codec/VtCallSpk.ini \
     vendor/samsung/galaxys2-common/proprietary/audio/lib/hw/acoustics.default.so:system/lib/hw/acoustics.default.so \
     vendor/samsung/galaxys2-common/proprietary/audio/lib/hw/alsa.default.so:system/lib/hw/alsa.default.so \
     vendor/samsung/galaxys2-common/proprietary/audio/usr/share/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
