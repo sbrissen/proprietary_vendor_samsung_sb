cfbba8447fe7e7c7e532d1d6cae3d0c37271ed2f
mauimauer <sebastian@n-unity.de>
2011-12-12 10:47:25 +0100

    ICS blobs

--- galaxynote/galaxynote-vendor-blobs.mk
+++ galaxynote/galaxynote-vendor-blobs.mk
@@ -13,12 +13,11 @@
 # limitations under the License.
 
 # Prebuilt libraries that are needed to build open-source libraries
-PRODUCT_COPY_FILES := \
-    vendor/samsung/galaxynote/proprietary/ril/lib/libril.so:obj/lib/libril.so \
-    vendor/samsung/galaxynote/proprietary/ril/lib/libsecril-client.so:obj/lib/libsecril-client.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudio.so:obj/lib/libaudio.so \
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libTVOut.so:obj/lib/libTVOut.so \
     vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamahaservice.so:obj/lib/libmediayamahaservice.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudiopolicy.so:obj/lib/libaudiopolicy.so
+	vendor/samsung/galaxynote/proprietary/ril/lib/libril.so:obj/lib/libril.so \
+    vendor/samsung/galaxynote/proprietary/ril/lib/libsecril-client.so:obj/lib/libsecril-client.so
 
 # EGL
 PRODUCT_COPY_FILES += \
@@ -31,31 +30,63 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxynote/proprietary/graphics/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/samsung/galaxynote/proprietary/graphics/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
     vendor/samsung/galaxynote/proprietary/graphics/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
-    vendor/samsung/galaxynote/proprietary/graphics/lib/hw/gralloc.smdkv310.so:system/lib/hw/gralloc.smdkv310.so
+    vendor/samsung/galaxynote/proprietary/graphics/lib/hw/gralloc.s5pc210.so:system/lib/hw/gralloc.s5pc210.so
 
 # HWCOMPOSER - needs tvout
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/hw/hwcomposer.smdkv310.so:system/lib/hw/hwcomposer.smdkv310.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/hw/hwcomposer.s5pc210.so:system/lib/hw/hwcomposer.s5pc210.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/bin/bintvoutservice:system/bin/bintvoutservice \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libcec.so:system/lib/libcec.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libddc.so:system/lib/libddc.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libedid.so:system/lib/libedid.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libfimc.so:system/lib/libfimc.so \
     vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libfimg.so:system/lib/libfimg.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvout.so:system/lib/libtvout.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutcec.so:system/lib/libtvoutcec.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutddc.so:system/lib/libtvoutddc.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutedid.so:system/lib/libtvoutedid.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/lib_tvoutengine.so:system/lib/lib_tvoutengine.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutfimc.so:system/lib/libtvoutfimc.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutfimg.so:system/lib/libtvoutfimg.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvouthdmi.so:system/lib/libtvouthdmi.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libhdmi.so:system/lib/libhdmi.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libhdmiclient.so:system/lib/libhdmiclient.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libTVOut.so:system/lib/libTVOut.so \
     vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvout_jni.so:system/lib/libtvout_jni.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutservice.so:system/lib/libtvoutservice.so \
-    vendor/samsung/galaxynote/proprietary/hwcomposer/bin/tvoutserver:system/bin/tvoutserver
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutinterface.so:system/lib/libtvoutinterface.so \
+    vendor/samsung/galaxynote/proprietary/hwcomposer/lib/libtvoutservice.so:system/lib/libtvoutservice.so
+	
+# CAMERA
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxynote/proprietary/camera/lib/hw/camera.s5pc210.so:system/lib/hw/camera.s5pc210.so \
+    vendor/samsung/galaxynote/proprietary/camera/lib/libcamera_client.so:system/lib/libcamera_client.so \
+    vendor/samsung/galaxynote/proprietary/camera/lib/libcaps.so:system/lib/libcaps.so \
+    vendor/samsung/galaxynote/proprietary/camera/lib/libs5pjpeg.so:system/lib/libs5pjpeg.so \
+	vendor/samsung/galaxynote/proprietary/camera/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    vendor/samsung/galaxynote/proprietary/camera/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv
+	
+# OMX
+#PRODUCT_COPY_FILES += \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomx263d.so:system/lib/libsomx263d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomx264d.so:system/lib/libsomx264d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxaacd.so:system/lib/libsomxaacd.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxac3d.so:system/lib/libsomxac3d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxamrd.so:system/lib/libsomxamrd.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxcmn.so:system/lib/libsomxcmn.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxcore.so:system/lib/libsomxcore.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxflacd.so:system/lib/libsomxflacd.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxmp3d.so:system/lib/libsomxmp3d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxmp4vd.so:system/lib/libsomxmp4vd.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxmp43d.so:system/lib/libsomxmp43d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxsr263d.so:system/lib/libsomxsr263d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxvc1d.so:system/lib/libsomxvc1d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxvenc.so:system/lib/libsomxvenc.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxvp8d.so:system/lib/libsomxvp8d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxwmad.so:system/lib/libsomxwmad.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxwmv7d.so:system/lib/libsomxwmv7d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libsomxwmv8d.so:system/lib/libsomxwmv8d.so \
+#    vendor/samsung/galaxynote/proprietary/omx/lib/libstagefright_omx.so:system/lib/libstagefright_omx.so
 	
 # SENSORS
 PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxynote/proprietary/sensors/lib/hw/lights.s5pc210.so:system/lib/hw/lights.s5pc210.so \
     vendor/samsung/galaxynote/proprietary/sensors/lib/libakm.so:system/lib/libakm.so
 	
 # GPS
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxynote/proprietary/gps/lib/hw/gps.GT-N7000.so:system/lib/hw/vendor-gps.smdkv310.so \
+    vendor/samsung/galaxynote/proprietary/gps/lib/hw/gps.s5pc210.so:system/lib/hw/gps.s5pc210.so
 	vendor/samsung/galaxynote/proprietary/gps/bin/gpsd:system/bin/gpsd
 	
 # WIFI
@@ -99,9 +130,28 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxynote/proprietary/lpm/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
     vendor/samsung/galaxynote/proprietary/lpm/media/Disconnected.qmg:system/media/Disconnected.qmg
 	
-# AUDIO
 PRODUCT_COPY_FILES += \
-	vendor/samsung/galaxynote/proprietary/audio/etc/audio/LVVEFS_Rx_Configuration.txt:system/etc/audio/LVVEFS_Rx_Configuration.txt \
+    vendor/samsung/galaxynote/proprietary/audio/lib/hw/alsa.default.so:system/lib/hw/alsa.default.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/hw/audio.primary.s5pc210.so:system/lib/hw/audio.primary.s5pc210.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/hw/audio_policy.s5pc210.so:system/lib/hw/audio_policy.s5pc210.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Acoustic_Module_Llite.so:system/lib/lib_Samsung_Acoustic_Module_Llite.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Sound_Booster.so:system/lib/lib_Samsung_Sound_Booster.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libasound.so:system/lib/libasound.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/liblvvefs.so:system/lib/liblvvefs.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamaha.so:system/lib/libmediayamaha.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamaha_jni.so:system/lib/libmediayamaha_jni.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamaha_tuning_jni.so:system/lib/libmediayamaha_tuning_jni.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamahaservice.so:system/lib/libmediayamahaservice.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libsamsungAcousticeq.so:system/lib/libsamsungAcousticeq.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libsamsungSoundbooster.so:system/lib/libsamsungSoundbooster.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/libyamahasrc.so:system/lib/libyamahasrc.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/soundfx/libbundlewrapper.so:system/lib/soundfx/libbundlewrapper.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/soundfx/libreverbwrapper.so:system/lib/soundfx/libreverbwrapper.so \
+    vendor/samsung/galaxynote/proprietary/audio/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so \
+    vendor/samsung/galaxynote/proprietary/audio/etc/audio/LVVEFS_Rx_Configuration.txt:system/etc/audio/LVVEFS_Rx_Configuration.txt \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/LVVEFS_Tx_Configuration.txt:system/etc/audio/LVVEFS_Tx_Configuration.txt \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt \
@@ -111,8 +161,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt \
     vendor/samsung/galaxynote/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt \
-	vendor/samsung/galaxynote/proprietary/audio/lib/hw/acoustics.default.so:system/lib/hw/acoustics.default.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/hw/alsa.default.so:system/lib/hw/alsa.default.so \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
@@ -129,24 +177,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
     vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
-    vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf \
-    vendor/samsung/galaxynote/proprietary/audio/lib/liba2dp.so:system/lib/liba2dp.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libasound.so:system/lib/libasound.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudio.so:system/lib/libaudio.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudioeffect_jni.so:system/lib/libaudioeffect_jni.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudiohw_op.so:system/lib/libaudiohw_op.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudiohw_sf.so:system/lib/libaudiohw_sf.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libaudiopolicy.so:system/lib/libaudiopolicy.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/liblvvefs.so:system/lib/liblvvefs.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamaha.so:system/lib/libmediayamaha.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamaha_jni.so:system/lib/libmediayamaha_jni.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libmediayamahaservice.so:system/lib/libmediayamahaservice.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libsamsungAcousticeq.so:system/lib/libsamsungAcousticeq.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Acoustic_Module_Llite.so:system/lib/lib_Samsung_Acoustic_Module_Llite.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libsamsungSoundbooster.so:system/lib/libsamsungSoundbooster.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/lib_Samsung_Sound_Booster.so:system/lib/lib_Samsung_Sound_Booster.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libsoundalive.so:system/lib/libsoundalive.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libsoundpool.so:system/lib/libsoundpool.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libSR_AudioIn.so:system/lib/libSR_AudioIn.so \
-    vendor/samsung/galaxynote/proprietary/audio/lib/libyamahasrc.so:system/lib/libyamahasrc.so
+    vendor/samsung/galaxynote/proprietary/audio/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
