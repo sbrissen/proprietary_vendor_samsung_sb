27cdee32e86d81b5d698efc9cdc859f261a4c129
jt1134 <jt1134@gmail.com>
2011-12-22 17:28:52 -0600

    vzwtab: don't use aries-common pvr blobs

--- vzwtab/vzwtab-vendor-blobs.mk
+++ vzwtab/vzwtab-vendor-blobs.mk
@@ -37,25 +37,21 @@ PRODUCT_COPY_FILES += \
 
 # gfx
 PRODUCT_COPY_FILES += \
-    vendor/samsung/aries-common/proprietary/pvrsrvinit:system/bin/pvrsrvinit \
-    vendor/samsung/aries-common/proprietary/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/samsung/aries-common/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
-    vendor/samsung/aries-common/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
-    vendor/samsung/aries-common/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
-    vendor/samsung/aries-common/proprietary/gralloc.aries.so:system/vendor/lib/hw/gralloc.s5pc110.so \
-    vendor/samsung/aries-common/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
-    vendor/samsung/aries-common/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
-    vendor/samsung/aries-common/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
-    vendor/samsung/aries-common/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
-    vendor/samsung/aries-common/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
-    vendor/samsung/aries-common/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
-    vendor/samsung/aries-common/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
-    vendor/samsung/aries-common/proprietary/libusc.so:system/vendor/lib/libusc.so \
-    vendor/samsung/aries-common/proprietary/libgralloc.so:system/vendor/lib/libgralloc.so
-
-# camera
-PRODUCT_COPY_FILES += \
-    vendor/samsung/aries-common/proprietary/camera.aries.so:system/vendor/lib/hw/camera.s5pc110.so
+    vendor/samsung/vzwtab/proprietary/pvrsrvinit:system/bin/pvrsrvinit \
+    vendor/samsung/vzwtab/proprietary/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    vendor/samsung/vzwtab/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/samsung/vzwtab/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/samsung/vzwtab/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/samsung/vzwtab/proprietary/gralloc.s5pc110.so:system/vendor/lib/hw/gralloc.s5pc110.so \
+    vendor/samsung/vzwtab/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+    vendor/samsung/vzwtab/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+    vendor/samsung/vzwtab/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+    vendor/samsung/vzwtab/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+    vendor/samsung/vzwtab/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+    vendor/samsung/vzwtab/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+    vendor/samsung/vzwtab/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+    vendor/samsung/vzwtab/proprietary/libusc.so:system/vendor/lib/libusc.so \
+    vendor/samsung/vzwtab/proprietary/libgralloc.so:system/vendor/lib/libgralloc.so
 
 # wifi
 PRODUCT_COPY_FILES += \
