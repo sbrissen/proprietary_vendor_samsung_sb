8f47e4213f651362005885f53d7dceb3a3fa0503
Andrew Mahone <andrew.mahone@gmail.com>
2012-01-27 05:26:54 -0500

    Install vendor binary camera library as vendor- for use by wrapper.

--- galaxys2-common/common-vendor-blobs.mk
+++ galaxys2-common/common-vendor-blobs.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # CAMERA
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxys2-common/proprietary/camera/lib/hw/camera.exynos4.so:system/lib/hw/camera.exynos4.so \
+    vendor/samsung/galaxys2-common/proprietary/camera/lib/hw/camera.exynos4.so:system/lib/hw/vendor-camera.exynos4.so \
     vendor/samsung/galaxys2-common/proprietary/camera/lib/libcamera_client.so:system/lib/libcamera_client.so \
     vendor/samsung/galaxys2-common/proprietary/camera/lib/libcaps.so:system/lib/libcaps.so \
     vendor/samsung/galaxys2-common/proprietary/camera/lib/libs5pjpeg.so:system/lib/libs5pjpeg.so
